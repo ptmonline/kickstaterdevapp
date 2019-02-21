d716ec0163a1feff3229a9de722a46594f39a0a2
ptmonline <ptmonlinework@gmail.com>
2019-02-21 14:45:51 +0100

    added more tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -13,7 +13,6 @@ let campaign;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
-    console.log(accounts);
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
@@ -35,9 +34,26 @@ beforeEach(async () => {
 });
 
 describe('Campaigns', ()=>{
+    require('events').EventEmitter.defaultMaxListeners = 0;
+    
     it('deploys a factory an a campaign', ()=>{
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async()=>{
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as approved', async()=>{
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
     });
 });
 
