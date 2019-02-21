9499618004873642af1db683e4b4f70e94346f80
ptmonline <ptmonlinework@gmail.com>
2019-02-21 15:09:35 +0100

    added end to end test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -33,20 +33,20 @@ beforeEach(async () => {
     );
 });
 
-describe('Campaigns', ()=>{
+describe('Campaigns', () => {
     require('events').EventEmitter.defaultMaxListeners = 0;
-    
-    it('deploys a factory an a campaign', ()=>{
+
+    it('deploys a factory an a campaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
     });
 
-    it('marks caller as the campaign manager', async()=>{
+    it('marks caller as the campaign manager', async () => {
         const manager = await campaign.methods.manager().call();
         assert.equal(accounts[0], manager);
     });
 
-    it('allows people to contribute money and marks them as approved', async()=>{
+    it('allows people to contribute money and marks them as approved', async () => {
         await campaign.methods.contribute().send({
             value: '200',
             from: accounts[1]
@@ -54,6 +54,57 @@ describe('Campaigns', ()=>{
 
         const isContributor = await campaign.methods.approvers(accounts[1]).call();
         assert(isContributor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err);
+        }
+    });
+
+    it('allows manageer to make a payment request', async () => {
+        await campaign.methods
+            .createRequest('Buy batteries', '100', accounts[1])
+            .send({
+                from: accounts[0],
+                gas: '1000000'
+            });
+
+        const request = await campaign.methods.requests(0).call();
+        assert.equal('Buy batteries', request.description);
+    });
+
+    it('processes requests', async () => {
+        await campaign.methods.contribute().send({
+            from: accounts[0],
+            value: web3.utils.toWei('10', 'ether')
+        });
+
+        await campaign.methods
+            .createRequest('Buy batteries', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' });
+
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        let balance = await web3.eth.getBalance(accounts[1]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+        console.log(balance);
+        assert(balance > 104);
     });
 });
 
