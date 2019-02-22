fc1d156abf8ed71a370160b9a079f7cd23648e6c
ptmonline <ptmonlinework@gmail.com>
2019-02-22 16:45:47 +0100

    added css

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import factory from '../ethereum/factory';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 
-class CampaignIndex extends Component{
+class CampaignIndex extends Component {
     // async componentDidMount(){
     //     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
@@ -11,25 +11,33 @@ class CampaignIndex extends Component{
 
     //We use some next.js methods
 
-    static async getInitialProps(){
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        return {campaigns: campaigns}
+        return { campaigns: campaigns }
     }
 
-    renderCampaigns(){
-        const items = this.props.campaigns.map(address=>{
-            return{
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
                 header: address,
                 description: <a>View Campaign</a>,
-                fluid: true 
+                fluid: true
             };
         });
 
         return <Card.Group items={items} />;
     }
 
-    render(){
-        return <div>{this.renderCampaigns()}</div>
+    render() {
+        return <div>
+            <link
+                rel="stylesheet"
+                href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+            />
+            <h3>Open Campaigns</h3>  
+            {this.renderCampaigns()}
+            <Button content="Create Campaign" icon="add circle" primary />
+        </div>;
     }
 }
 
