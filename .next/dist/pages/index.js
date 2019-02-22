24ae865b710cb7201aef602802e6b26e9bb95b7e
ptmonline <ptmonlinework@gmail.com>
2019-02-22 10:07:33 +0100

    added pages folder + next logic

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/home/pau/Documents/personal/kickstaterdevapp/pages/index.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, 'This is the campaign list page');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQUksQUFDZjsyQkFDSSxjQUFBOztzQkFBQTt3QkFBQTtBQUFBO0FBQUEsS0FBQSxFQURKLEFBQ0ksQUFFUDtBQUpEIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9ob21lL3BhdS9Eb2N1bWVudHMvcGVyc29uYWwva2lja3N0YXRlcmRldmFwcCJ9
