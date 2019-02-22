83b16d67c25f0db4c916208eb31a5730ff7d9dfb
ptmonline <ptmonlinework@gmail.com>
2019-02-22 15:20:08 +0100

    changed ethereum provider

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // we are in the browser and metamask is running.
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // we are on the server *OR* the user is not running metamask.
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/4404778bc8d64811b4f531e508d9817c'
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
