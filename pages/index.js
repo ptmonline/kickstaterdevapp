273ef9c3e470750f5b6a140d384331047961ceb0
ptmonline <ptmonlinework@gmail.com>
2019-02-22 16:24:20 +0100

    added sematic ui react

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component{
-    async componentDidMount(){
-        const campaigns = await factory.methods.getDeployedCampaigns().call();
+    // async componentDidMount(){
+    //     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        console.log(campaigns);
+    //     console.log(campaigns);
+    // }
+
+    //We use some next.js methods
+
+    static async getInitialProps(){
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return {campaigns: campaigns}
+    }
+
+    renderCampaigns(){
+        const items = this.props.campaigns.map(address=>{
+            return{
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true 
+            };
+        });
+
+        return <Card.Group items={items} />;
     }
 
     render(){
-        return <div>Campaigns Index!</div>
+        return <div>{this.renderCampaigns()}</div>
     }
 }
 
