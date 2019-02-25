7d6d16e6e99a4345f2ea0981fd277adcc629e2e8
ptmonline <ptmonlinework@gmail.com>
2019-02-25 16:08:30 +0100

    added campaign new form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
-import Layout from '../components/Layout.js';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component {
     // async componentDidMount(){
@@ -33,13 +33,9 @@ class CampaignIndex extends Component {
         return (
             <Layout>
                 <div>
-                    <link
-                        rel="stylesheet"
-                        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-                    />
                     <h3>Open Campaigns</h3>
+                    <Button floated="right" content="Create Campaign" icon="add circle" primary />
                     {this.renderCampaigns()}
-                    <Button content="Create Campaign" icon="add circle" primary />
                 </div>
             </Layout>
         );
