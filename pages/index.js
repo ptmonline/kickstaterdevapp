80c7c61d4b9172928cf347a9498b21fd1ef29499
ptmonline <ptmonlinework@gmail.com>
2019-02-26 16:21:02 +0100

    added routing to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
 import Layout from '../components/Layout';
+import { Link } from '../routes';
 
 class CampaignIndex extends Component {
     // async componentDidMount(){
@@ -21,7 +22,7 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: <Link route={`/campaigns/${address}`}><a>View Campaign</a></Link>,
                 fluid: true
             };
         });
@@ -34,7 +35,9 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open Campaigns</h3>
-                    <Button floated="right" content="Create Campaign" icon="add circle" primary />
+                    <Link route="/campaigns/new">
+                        <a><Button floated="right" content="Create Campaign" icon="add circle" primary /></a>
+                    </Link>
                     {this.renderCampaigns()}
                 </div>
             </Layout>
