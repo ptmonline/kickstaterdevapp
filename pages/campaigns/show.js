23da4c2d33ebc7c064703a925ef9b9632dba1b2a
ptmonline <ptmonlinework@gmail.com>
2019-02-27 10:31:42 +0100

    added Card design

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import Campaign from '../../ethereum/campaign'; 
+import Campaign from '../../ethereum/campaign';
+import { Card } from 'semantic-ui-react';
 
 class CampaignShow extends Component {
-    static async getInitialProps(props){
+    static async getInitialProps(props) {
         const campaign = Campaign(props.query.address);
 
         const summary = await campaign.methods.getSummary().call();
-        console.log(summary);
-        return {};
+
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
+    }
+
+    renderCards() {
+        const { balance, manager, minimumContribution, requestCount, approversCount } = this.props;
+        const items = [
+            {
+                header: manager,
+                meta: 'Address of Manager',
+                description: 'The manager created this campaign and can create requests to withdraw money',
+                style: {overflowWrap: 'break-word'}
+            }
+        ];
+
+        return <Card.Group items={items} />;
     }
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
         );
     }
