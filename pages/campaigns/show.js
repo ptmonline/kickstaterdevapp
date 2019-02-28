ed891d3bb17f17bb1f67abfd0282c2fd85158e51
ptmonline <ptmonlinework@gmail.com>
2019-02-28 16:10:53 +0100

    added input field to Contribute Form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
 
@@ -12,6 +12,7 @@ class CampaignShow extends Component {
         const summary = await campaign.methods.getSummary().call();
 
         return {
+            address: props.query.address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestCount: summary[2],
@@ -57,8 +58,10 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <h3>Campaign Details</h3>
-                {this.renderCards()}
-                <ContributeForm />
+                <Grid>
+                    <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+                    <Grid.Column width={6}><ContributeForm address={this.props.address} /></Grid.Column>
+                </Grid>
             </Layout>
         );
     }
