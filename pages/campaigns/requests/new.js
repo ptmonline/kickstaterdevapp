5110aa40ffd5b135960335452e736ba470cf8f3f
ptmonline <ptmonlinework@gmail.com>
2019-03-01 14:55:25 +0100

    worked on Requests Row

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -10,7 +10,9 @@ class RequestNew extends Component {
     state = {
         value: '',
         description: '',
-        recipient: ''
+        recipient: '',
+        loading: false,
+        errorMessage: ''
     }
 
     static async getInitialProps(props) {
@@ -23,22 +25,31 @@ class RequestNew extends Component {
         const campaign = await Campaign(this.props.address);
         const { description, value, recipient } = this.state;
 
+        this.setState({ loading: true, errorMessage: '' });
+
         try {
             const accounts = await web3.eth.getAccounts();
             await campaign.methods.createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
                 .send({
                     from: accounts[0]
                 });
-        } catch (err) {
 
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
         };
+
+        this.setState({ loading: false });
 
     }
     render() {
         return (
             <Layout>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>Back</a>
+                </Link>
                 <h3>Create a Request</h3>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Description</label>
                         <Input
@@ -62,8 +73,8 @@ class RequestNew extends Component {
                             onChange={event => this.setState({ recipient: event.target.value })}
                         />
                     </Form.Field>
-
-                    <Button primary>Create!</Button>
+                    <Message error header='Oops!' content={this.state.errorMessage} />
+                    <Button primary loading={this.state.loading}>Create!</Button>
                 </Form>
             </Layout>
         );
